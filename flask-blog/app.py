9e3f9f366614f1bf0e1544c10d7a4dd5138a21b8
maniharafiqkr-hue <maniha.rafiq.kr@gmail.com>
2026-03-13 11:17:15 +0900

    feat: add home endpoints

---
+++ flask-blog/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return "<h1>Welcome to My Flask Blog!</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
